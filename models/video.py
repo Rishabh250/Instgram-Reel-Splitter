8c792ec163da806a7eb97b47606b632e00c85e0c
Rishabh Bansal <rbansal@lorienfinance.com>
2024-11-19 12:17:49 +0530

    IRS-001-setup-fastapi

--- models/video.py
+++
@@ -1,9 +0,0 @@
-from bson.objectid import ObjectId
-
-def save_metadata(metadata):
-    """Save video metadata to MongoDB."""
-    return videos_collection.insert_one(metadata).inserted_id
-
-def get_video_metadata(video_id):
-    """Retrieve video metadata from MongoDB."""
-    return videos_collection.find_one({"_id": ObjectId(video_id)}) 
